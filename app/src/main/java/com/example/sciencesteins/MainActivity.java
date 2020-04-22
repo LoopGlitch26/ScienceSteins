7e3403f6edcc45a5caf03be3ef84008e08cecdb4
LoopGlitch_ <grandson.baba.2012@gmail.com>
2020-04-22 09:30:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sciencesteins/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.sciencesteins;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://sciencesteins2020.wixsite.com/master");
+        myWebView.setWebViewClient(new WebViewClient());
+    }
+    @Override
+    public void onBackPressed() {
+        if(myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
